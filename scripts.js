737b574252adb986e60e9af09f856885ddc52e03
ademiando <ademiandoo@gmail.com>
2025-05-02 18:29:11 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
 
-// Tambahkan interaksi untuk tombol favorit
-document.querySelectorAll('.favorite-button').forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const row = event.target.closest('.row');
-    const mountainName = row.querySelector('.mountain-info strong').textContent;
-    alert(`${mountainName} has been added to your favorites!`);
-  });
-});
